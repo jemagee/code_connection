3a19febb55fc77981e363e0f77b4a26a4e8b0ee9
John Magee <jemagee@cox.net>
2017-03-25 11:58:24 -0700

    Adding admin attribute as boolean to user, with default false setting

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 	it {should validate_presence_of(:email)}
+
+	describe "Admin default value should be false" do
+
+		let(:user) {User.new}
+
+		it "should have a default admin of false" do
+			expect(user.admin).to eq false
+		end
+	end
 end
