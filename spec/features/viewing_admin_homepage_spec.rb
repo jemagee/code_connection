c13e28cdedce717ee500dbb771d70e7c3cb4b3e2
John Magee <jemagee@cox.net>
2017-05-15 18:30:45 -0700

    Expanding the test on the access to the admin home page for logged in admins and non logged in users

--- spec/features/viewing_admin_homepage_spec.rb
+++ spec/features/viewing_admin_homepage_spec.rb
@@ -17,4 +17,27 @@ RSpec.feature "Testing Access to the admin homepage" do
 			expect(page).to have_content("You do not have access to that page!")
 		end
 	end
+
+	context "When logged in as the admin" do
+
+		before {login_as(admin)}
+
+		scenario "Can access the home page" do
+
+			visit admin_root_path
+
+			expect(current_path).to eq admin_root_path
+			expect(page).to_not have_content("You do not have access to that page!")
+		end
+	end
+
+	context "When not logged in at all" do
+
+		scenario "Gets sent to the sign in page" do
+
+			visit admin_root_path
+			
+			expect(current_path).to eq new_user_session_path
+		end
+	end
 end
