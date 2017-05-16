ea45a16b0fcc69ee0bddb6dfecc941a92784c56b
John Magee <jemagee@cox.net>
2017-05-15 19:00:07 -0700

    Basic Skill addition creation and testing behind admin name space

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :admin do
     root 'application#index'
+    resources :skills
   end
 
   root 'static_pages#home'
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170413093153) do
+ActiveRecord::Schema.define(version: 20170516015252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "citext"
+
+  create_table "skills", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
