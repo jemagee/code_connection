9edc2c208887857d77d94216eee0d281fb294acd
John Magee <jemagee@cox.net>
2017-05-16 19:03:11 -0700

    Creating ability to delete non admin users from the Users Index Page

--- spec/features/admin/viewing_users_spec.rb
+++ spec/features/admin/viewing_users_spec.rb
@@ -4,9 +4,12 @@ RSpec.feature "Viewing the Users Index" do
 
 	let(:admin) {create(:user, admin: true)}
 	let!(:user) {create(:user)}
+	let!(:admin2) {create(:user, admin: true)}
 
 	before do
 		login_as(admin)
+		visit admin_root_path
+		click_link "Users Index"
 	end
 
 	scenario "From the admin root page" do
@@ -17,6 +20,28 @@ RSpec.feature "Viewing the Users Index" do
 		expect(current_path).to eq admin_users_path
 		expect(page).to have_content("List of Users")
 		expect(page).to have_content(user.username)
+		expect(page).to have_content(admin.username)
+	end
+
+	scenario "THere is a delete link for non admin users" do
+
+		within("div##{user.id}") do
+			expect(page).to have_link "Delete User"
+		end
+	end
+
+	scenario "There is not a delete link for logged in admin user" do
+
+		within("div##{admin.id}") do
+			expect(page).to_not have_content("Delete User")
+		end
+	end
+
+	scenario "There is not a delete link for not logged in admin user" do
+
+		within("div##{admin2.id}") do
+			expect(page).to_not have_content("Delete User")
+		end
 	end
 end
 
