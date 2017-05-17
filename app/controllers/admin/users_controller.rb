f2f54549211ef48b866f1056fdb576f9ce935193
John Magee <jemagee@cox.net>
2017-05-16 20:11:28 -0700

    Refactoring admin user controller for before_action to get user, refactoring admin edit form and controller to prevent password modification

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,25 +1,20 @@
 class Admin::UsersController < Admin::ApplicationController
+
+	before_action :get_user, only: [:edit, :destroy, :update]
   def index
   	@users = User.all
   end
 
   def edit
-  	@user = User.find(params[:id])
   end
 
   def destroy
-  	@user = User.find(params[:id])
   	@user.destroy
   	flash[:success] = "User Successfully Deleted!"
   	redirect_to admin_users_path
   end
 
   def update
-  	@user = User.find(params[:id])
-  	if params[:user][:password].blank?
-  		params[:user].delete(:password)
-  		params[:user].delete(:password_confirmation)
-  	end
   	if @user.update(user_params)
   		flash[:success] = "User Successfully Updated!"
   		redirect_to admin_root_path
@@ -32,6 +27,10 @@ class Admin::UsersController < Admin::ApplicationController
   private 
 
   	def user_params
-  		params.require(:user).permit(:email, :username, :password, :password_confirmation)
+  		params.require(:user).permit(:email, :username)
+  	end
+
+  	def get_user
+  		@user = User.find(params[:id])
   	end
 end
