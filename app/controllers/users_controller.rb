c42486ea2f84ced6dd3006e26c276688a9e1d9cb
John Magee <jemagee@cox.net>
2017-08-08 19:17:21 -0700

    Altering the show action and testing as anyone should be able to see a profile (for now) but not everyone should have access to the editing link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,9 @@
 class UsersController < ApplicationController
 
 	def show
-		if params[:id].to_i == current_user.id
-			@user = User.find(params[:id])
-		else
-			flash[:danger] = "You do not have access to that page"
-			redirect_to root_path
-		end
+		@user = User.find(params[:id])
+	end
+
+	def edit
 	end
 end
--- spec/features/user_profile_page_spec.rb
+++ spec/features/user_profile_page_spec.rb
@@ -14,18 +14,19 @@ RSpec.feature "The User Profile Page" do
 			visit user_path(user)
 
 			expect(page).to have_content("Profile page of #{user.username}")
+			expect(page).to have_link("Edit Your Profile")
 		end
 	end
 
-	context "Logged in as the wrong user" do
+	context "Logged in as another user" do
 
 		before {login_as(user2)}
 
-		scenario "can not be viewed" do
+		scenario "can not view edit link" do
 
 			visit user_path(user)
-			expect(page).to have_content("You do not have access to that page")
-			expect(page).to_not have_content("Profile page of #{user.username}")
+			expect(page).to have_content("Profile page of #{user.username}")
+			expect(page).to_not have_link("Edit Your Profile")
 		end
 	end
 end
